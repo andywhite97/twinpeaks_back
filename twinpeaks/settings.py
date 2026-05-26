5981bbdc5e4745fb6fa063fb05252d5f05234e6b
Andy <90454644+andywhite97@users.noreply.github.com>
2026-05-26 23:20:40 +0200

    Configure backend for Render deployment

--- twinpeaks/settings.py
+++ twinpeaks/settings.py
@@ -197,7 +197,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Static files directories (for development)
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+] if (BASE_DIR / 'static').exists() else []
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
