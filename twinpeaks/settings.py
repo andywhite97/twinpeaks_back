811e1a3e2b901a19309d8e50d7a98a64431db510
Andy <90454644+andywhite97@users.noreply.github.com>
2026-05-27 14:07:32 +0200

    Configure backend for Render deployment

--- twinpeaks/settings.py
+++ twinpeaks/settings.py
@@ -197,8 +197,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Static files directories (for development)
 STATICFILES_DIRS = [
