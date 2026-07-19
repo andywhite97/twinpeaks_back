2885d14be5e2b5cf10565179a7539e2863edcba3
Andy <90454644+andywhite97@users.noreply.github.com>
2026-07-19 19:31:17 +0200

    Update views.py

--- contact/views.py
+++ contact/views.py
@@ -4,8 +4,7 @@ from rest_framework.throttling import AnonRateThrottle
 from django.conf import settings
 from .models import ContactMessage
 from .serializers import ContactMessageSerializer
-
-from bird import APIError, Bird
+from .tasks import send_contact_message_notification
 
 
 class ContactMessageCreateView(CreateAPIView):
@@ -17,18 +16,7 @@ class ContactMessageCreateView(CreateAPIView):
     def perform_create(self, serializer):
         message = serializer.save()
 
-        msg = ContactMessage.objects.get(pk=message.id)
-        with Bird() as client:
-            try:
-                message = client.email.send(
-                    from_={"email": "info@twinpeaksinvestment.com", "name": "TwinPeaks Investments"},
-                    to=["andileblessinghlophe@gmail.com"],
-                    subject=f"New Contact Message from {msg.name}",
-                    html=f"<p>{msg.message}</p>",
-                )
-                print(message.id, message.status)
-            except APIError as err:
-                print("send failed:", err)
+        send_contact_message_notification(message)
 
 
 class ContactMessageListView(ListAPIView):
